e351cf3e91e0ec043680533e58bee9005e55ad08
Santiago Marcano <marcanomedia941@gmail.com>
2021-04-21 19:19:24 +0200

    Theme integration + Exploring providers (Web3 is a must)

---
+++ src/pages/documentation/DocsDownload.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { Row, Col, Card, Container } from "@themesberg/react-bootstrap";
+
+export default () => (
+  <Container className="px-0">
+    <Row>
+      <Col xs={12} className="p-3">
+        <Card>
+          <Card.Body>
+            <article>
+              <h1 className="h2" id="download">
+                Download{" "}
+              </h1>
+              <p className="fs-5 fw-light">
+                Download files for Common Sense Dashboard
+              </p>
+              <p>
+                Use the following links to download Common Sense Dashboard
+                files:
+              </p>
+              <ul className="docs-list">
+                <li>
+                  Download{" "}
+                  <Card.Link
+                    href="https://themesberg.com/product/dashboard/volt-pro-react"
+                    target="_blank"
+                  >
+                    Common Sense Dashboard
+                  </Card.Link>
+                  .
+                </li>
+              </ul>
+            </article>
+          </Card.Body>
+        </Card>
+      </Col>
+    </Row>
+  </Container>
+);
